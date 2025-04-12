ae4fa045fe053fa4aa825d4eb3f812d7e3f732f1
pHervasC <phervascalvet@gmail.com>
2025-04-12 17:10:20 +0200

    cambios para hacer mas rapida la apertura del sobre

--- src/app/component/sobre/sobre.abrir.routed/sobre.abrir.routed.component.ts
+++ src/app/component/sobre/sobre.abrir.routed/sobre.abrir.routed.component.ts
@@ -90,7 +90,7 @@ export class SobreAbrirRoutedComponent implements OnInit, AfterViewInit {
             }
           });
         }
-        // Esperamos 3 segundos para que termine la animación del sobre
+
         setTimeout(() => {
           cartas.forEach((carta: any, index: number) => {
             setTimeout(() => {
@@ -101,13 +101,13 @@ export class SobreAbrirRoutedComponent implements OnInit, AfterViewInit {
                 },
                 error: () => {}
               });
-            }, index * 500); // Cada carta aparecerá con 500ms de diferencia
+            }, index * 250);
           });
 
           setTimeout(() => {
             this.mostrarBotonAbrir = true;
-          }, cartas.length * 500 + 1000);
-        }, 3000); // Esperamos 3 segundos para que termine la animación del sobre
+          }, cartas.length * 250 + 500);
+        }, 1500);
       },
       error: (error) => {
         const mensaje = error.error?.error || "No se pudo abrir el sobre.";
