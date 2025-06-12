ea966075561e6a1686c99e9de0798e4c2187f6d0
pHervasC <phervascalvet@gmail.com>
2025-06-12 11:57:45 +0200

    a por router link

--- src/app/component/shared/shared.home.routed/shared.home.routed.component.ts
+++ src/app/component/shared/shared.home.routed/shared.home.routed.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-shared-home-routed',
   templateUrl: './shared.home.routed.component.html',
-  styleUrls: ['./shared.home.routed.component.css']
+  styleUrls: ['./shared.home.routed.component.css'],
+   imports: [
+      RouterModule
+    ]
 })
 export class SharedHomeRoutedComponent implements OnInit {
 
